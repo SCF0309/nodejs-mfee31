6cbe95a7f41015f637810e757eaccee82dbef7bd
Allenkuo <scf0309@yahoo.com.tw>
2022-12-11 16:48:13 +0800

    add lot of things

---
+++ crawler/crawler3.js
@@ -0,0 +1,32 @@
+// axios await 版本
+// 把 query string 抽出來當變數，用 params 的方式去設定
+
+// 1. 安裝 npm i axios
+// 2. 引用 require
+// 3. 去讀官方文件
+const axios = require('axios');
+// http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
+const fs = require('fs/promises');
+const moment = require('moment');
+//fs= filesystem
+
+(async () => {
+  try {        
+    let stockNo = await fs.readFile('stock.txt', 'utf-8');
+    console.log(stockNo);
+    let date = moment().format('YYYYMMDD');
+    console.log(date);
+
+    let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
+      params: {
+        stockNo,
+        date,
+      },
+    });
+
+    console.log('await', response.data);
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
